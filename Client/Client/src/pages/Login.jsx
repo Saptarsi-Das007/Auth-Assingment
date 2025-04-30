1bc2f0aa28079fbe2deaeeac65ac0c94d3f37a2e
Saptarsi-Das007 <dassaptarsi2@gmail.com>
2025-04-30 18:59:47 +0530

    Update Login.jsx

--- Client/Client/src/pages/Login.jsx
+++ Client/Client/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8001/api/auth/login", {
+      const res = await axios.post("https://auth-assingment-backend.onrender.com/api/auth/login", {
         email,
         password,
       });
